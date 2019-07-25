17c02e6d5ccc849d0892cbd5fb4a6109fa14bb17
kondareddygudipati <gudipati.appstore@gmail.com>
2019-07-24 21:19:51 -0400

    AndroidX migration

--- app/src/main/java/com/gcoders/schoolinfo/schoolsfindersystem/view/SchoolMainActivity.java
+++ app/src/main/java/com/gcoders/schoolinfo/schoolsfindersystem/view/SchoolMainActivity.java
@@ -2,10 +2,10 @@ package com.gcoders.schoolinfo.schoolsfindersystem.view;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.appcompat.widget.Toolbar;
 import android.widget.Toast;
 
 import com.gcoders.schoolinfo.schoolsfindersystem.R;
--- app/src/main/java/com/gcoders/schoolinfo/schoolsfindersystem/view/SplashActivity.java
+++ app/src/main/java/com/gcoders/schoolinfo/schoolsfindersystem/view/SplashActivity.java
@@ -2,7 +2,7 @@ package com.gcoders.schoolinfo.schoolsfindersystem.view;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.widget.Toast;
 
 import com.gcoders.schoolinfo.schoolsfindersystem.R;
